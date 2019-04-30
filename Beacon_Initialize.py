64f55175c284deaf8c3a26353704892ccc66c102
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2019-04-30 10:54:03 -0500

    Update Beacon_Initialize.py

--- Beacon_Initialize.py
+++ Beacon_Initialize.py
@@ -1,1 +1,32 @@
+import bsmLib
 from bsmLib import RPL
+from bsmLib import tcpServer
+from bsmLib import vector 
+
+roscore
+cd ~/catkin_ws
+source devel/setup.bash
+ls /dev/ttyACM*
+rosrun marvelmind_nav hedge_rcv_bin /dev/ttyACM1
+
+tcpServer(port = '10003')
+
+t = tcpServer()
+t.listen()
+
+vec = vector()
+
+def c_read():
+    coords = t.recv()
+
+    startx = coords.find( 'X=' )
+    midy = coords.find( 'Y=' )
+    endz = coords.find( 'Z=' )
+
+    if start != -1 and end != -1:
+        result = coords[startx+1:midy+1:endz]
+
+    lat, long, height = result.split
+
+    vec.set(lat, long)
+    return vec
